321fa2ee96542f107242d0aff35c53dcad74665d
Domjan Dominika <domjan.dominika@simonyiszki.org>
2025-09-27 11:36:51 +0200

    Kezdőlap / 404 page route beállítva

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Landing}, //kezdőoldal
+    { path: '**', redirectTo: ''} //404 -> kezdőoldal
+];
