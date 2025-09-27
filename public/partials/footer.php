e8ff448fdaeaf19f38f2f340381d8a7425fd6aa4
domjandominika <domjan.dominika@simonyiszki.org>
2025-09-27 11:08:48 +0200

    Delete public directory
    Ez is php ami már nem kell

--- public/partials/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="mt-auto py-4 text-center text-white-50">
-  <div class="container small">© <?php echo date('Y'); ?> MentorMeet</div>
-</footer>
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
