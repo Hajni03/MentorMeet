18924f316db3bcecde797cac071d1a45a7744472
Domjan Dominika <domjan.dominika@simonyiszki.org>
2025-09-27 12:09:33 +0200

    Header elkezdése, RouterLink, RouterLinkActive + szülő komponensbe beimportáltam a gyerek komponenseket

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
 
+//layout
+import { Header } from '../../layout/header/header';
+import { Footer } from '../../layout/footer/footer';
+
+//sections
+import { Hero } from '../../sections/hero/hero';
+import { BrandStrip } from '../../sections/brand-strip/brand-strip';
+import { Features } from '../../sections/features/features';
+import { TestimonialsStrip } from '../../sections/testimonials-strip/testimonials-strip';
+import { HowItWorks } from '../../sections/how-it-works/how-it-works';
+import { Cta } from '../../sections/cta/cta';
+
 @Component({
   selector: 'app-landing',
-  imports: [],
+  standalone: true,
+  imports: [
+    Header,
+    Hero,
+    BrandStrip,
+    Features,
+    TestimonialsStrip,
+    HowItWorks,
+    Cta,
+    Footer
+  ],
   templateUrl: './landing.html',
   styleUrl: './landing.scss'
 })
