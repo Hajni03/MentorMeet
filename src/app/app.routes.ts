e1c5ac26e57f3e731dc487df694ac24f7ed2a7a8
Domjan Dominika <domjan.dominika@simonyiszki.org>
2025-10-06 11:19:49 +0200

    Regisztráció komponens létrehozva

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {path: '', component: Landing}, //kezdőoldal
-    { path: '**', redirectTo: ''} //404 -> kezdőoldal
+    {path: '**', redirectTo: ''}, //404 -> kezdőoldal
+    {path: 'register', component: Register}
 ];
