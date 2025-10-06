a1892aea88eefd8dad046d8cdd491eb94abd0dd2
Domjan Dominika <domjan.dominika@simonyiszki.org>
2025-10-06 11:31:18 +0200

    regisztrációs útvonal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {path: '', component: Landing}, //kezdőoldal
+    {path: 'register', component: Register}, //regisztrációs oldal útvonal
     {path: '**', redirectTo: ''}, //404 -> kezdőoldal
-    {path: 'register', component: Register}
+    
 ];
