b02239b0d2c4d3008548d892e9bba7d4706cf248
Domjan Dominika <domjan.dominika@simonyiszki.org>
2025-11-07 11:37:58 +0100

    Javítások+ új szekció, de még nincs kész

--- src/app/sections/how-it-works/how-it-works.ts
+++ src/app/sections/how-it-works/how-it-works.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Step = {
+  title: string;
+  desc: string;
+  img: string;
+  alt: string;
+};
 
 @Component({
   selector: 'app-how-it-works',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './how-it-works.html',
   styleUrl: './how-it-works.scss'
 })
 export class HowItWorks {
-
+  steps: Step[] = [
+    {
+      title: 'Regisztráció',
+      desc: 'Hozz létre egy fiókot pár kattintással, és lépj be a rendszerbe.',
+      img: 'assets/img/icons/profile_icon.png', // vagy .svg – ikon kép ide
+      alt: 'Felhasználó ikon'
+    },
+    {
+      title: 'Tanár kiválasztása',
+      desc: 'Találd meg a számodra megfelelő tanárt a listából.',
+      img: 'assets/img/icons/teacher_icon.png',
+      alt: 'Tanár ikon'
+    },
+    {
+      title: 'Időpontfoglalás',
+      desc: 'Válassz egy szabad időpontot, és készülj a konzultációra.',
+      img: 'assets/img/icons/calendar_icon.png',
+      alt: 'Naptár ikon'
+    }
+  ];
 }
